0c1127f3ad88047b2d7c3f29888fcb002069cc7c
sageshoyu <seiji.shaw@gmail.com>
2020-06-23 23:05:57 -0700

    add more collision control points to frame rmp

--- rmp_example_frame.py
+++ rmp_example_frame.py
@@ -3,7 +3,7 @@ import motor_skills.core.mj_control as mjc
 from motor_skills.envs.mj_jaco import MjJacoEnv
 from motor_skills.rmp.kdl_rmp import ProjectionNode
 from urdf_parser_py.urdf import URDF as u_parser
-from motor_skills.rmp.kdl_rmp import rmp_from_urdf, PositionProjection, KDLRMPNode, kdl_node_array
+from motor_skills.rmp.kdl_rmp import rmp_from_urdf, PositionProjection, KDLRMPNode, kdl_node_array, RotZProjection, RotYProjection, RotXProjection
 import motor_skills.rmp.rmp_leaf as leaves
 
 # %%
@@ -46,10 +46,29 @@ collision_pts_pos = {}
 for name, node in links.items():
     collision_pts_pos[name] = PositionProjection(name + "_pos", node)
 
+
+# add line of collision points along forearm
+link4_proj = ProjectionNode("link4_proj", root, np.array([1] * 4 + [0] * 8))
+link4_exts = kdl_node_array("link4_ext", link4_proj, robot, 'world', 'j2s6s300_link_4',
+                           spacing=0.05, skip=1, num=5, link_dir=np.array([0,0,1]).reshape(-1, 1))
+link4_exts_pos = [PositionProjection(link4_ext.name + "_pos", link4_ext) for link4_ext in link4_exts]
+for forearm_pos in link4_exts_pos:
+    collision_pts_pos[forearm_pos.name] = forearm_pos
+
+# add additional collision point at wrist
+link5_proj = ProjectionNode("link5_proj", root, np.array([1] * 5 + [0] * 7))
+link5_ext = KDLRMPNode("link5_ext", link5_proj, robot, 'world', 'j2s6s300_link_5',
+                        offset=np.array([0, 0.5, 0]).reshape(-1, 1))
+link5_ext_pos = PositionProjection("link5_ext_pos", link5_ext)
+collision_pts_pos[link5_ext_pos.name] = link5_ext_pos
+
 # add line of position nodes through hand to dictionary
 link6_proj = ProjectionNode("link6_proj", root, np.array([1] * 6 + [0] * 6))
 link6_exts = kdl_node_array("link6_ext", link6_proj, robot, 'world', 'j2s6s300_link_6',
                             spacing=0.05, skip=1, num=3, link_dir=np.array([0, 0, -1]).reshape(-1, 1))
+link6_rotx = RotXProjection("link6_rotx", link4_exts[1])
+link6_roty = RotYProjection("link6_roty", link4_exts[1])
+link6_rotz = RotZProjection("link6_rotz", link4_exts[1])
 link6_exts_pos = [PositionProjection(link6_ext.name + "_pos", link6_ext) for link6_ext in link6_exts]
 
 # add to collision points position too
@@ -61,19 +80,19 @@ for hand_pos in link6_exts_pos:
 for name, node in list(collision_pts_pos.items()):
     leaves.CollisionAvoidanceBox(name + "_right_avoider", node, None,
                                  np.array([fright_pos]).T, np.array([[0.05, 0.05, 0.10]]).T,
-                                 0.005, epsilon=0.0, eta=0, r_w = 0.10)
+                                 0.005, epsilon=0.0, eta=0, r_w = 0.15, alpha=1e-5)
 
     leaves.CollisionAvoidanceBox(name + "_left_avoider", node, None,
                                  np.array([fleft_pos]).T, np.array([[0.05, 0.05, 0.10]]).T,
-                                 0.005, epsilon=0.0, eta=0, r_w = 0.10)
+                                 0.005, epsilon=0.0, eta=0, r_w = 0.15, alpha=1e-5)
 
     leaves.CollisionAvoidanceBox(name + "_bot_avoider", node, None,
                                  np.array([fbot_pos]).T, np.array([[0.1, 0.05, 0.05]]).T,
-                                 0.005, epsilon=0.0, eta=0, r_w = 0.10)
+                                 0.005, epsilon=0.0, eta=0, r_w = 0.15, alpha=1e-5)
 
     leaves.CollisionAvoidanceBox(name + "_top_avoider", node, None,
                                  np.array([ftop_pos]).T, np.array([[0.1, 0.05, 0.05]]).T,
-                                 0.005, epsilon=0.0, eta=0, r_w=0.10)
+                                 0.005, epsilon=0.0, eta=0, r_w=0.15, alpha=1e-5)
 
 # attract palm of hand to target
 atrc = leaves.GoalAttractorUni("jaco_attractor", link6_exts_pos[1], np.array([target_pos]).T, gain=20)
